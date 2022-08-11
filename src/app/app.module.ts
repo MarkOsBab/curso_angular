98d0507e308a6e5bc54a2f72b35e7acc2d702013
MarkOsBab <m.g4089188@gmail.com>
2022-08-11 14:21:36 -0300

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeroesModule } from './heroes/heroes.module';
-import { ContadorModule } from './contador/contador.';
+import { ContadorModule } from './contador/contador.module';
 
 @NgModule({
   declarations: [
